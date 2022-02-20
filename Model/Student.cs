c5583f35ea5d42137dcf97d453fceebbc2695fcf
78702 <78702@FANG-SH>
2022-02-20 18:49:49 +0800

    常皓创建了student类

---
+++ Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+   public  class Student
+    {
+        public int id { get; set; }
+        public int name { get; set; }
+        public int age { get; set; }
+    }
+}
