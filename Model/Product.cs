47f21f9da3d25bfbe019533c11ea2fb64f976c78
俊杰 冯 <2623443095@qq.com>
2022-02-20 19:05:49 +0800

    冯俊杰创建了一个Product

---
+++ Model/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Product
+    {
+        /// <summary>
+        /// 主键id
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+        /// <summary>
+        /// 商品名称
+        /// </summary>
+        public string ProductName { get; set; }
+        /// <summary>
+        /// 商品单价
+        /// </summary>
+        public Decimal ProductPrice { get; set; }
+    }
+}
