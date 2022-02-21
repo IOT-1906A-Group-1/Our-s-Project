9f9ce1e5174c54c9caabb0bb6abf3f7ef144927d
mike Yang <15802994845@163.com>
2022-02-21 09:35:18 +0800

    杨泽霖在UModel添加了Attr字段

--- Model/UModel.cs
+++ Model/UModel.cs
@@ -13,5 +13,6 @@ namespace Model
         public int Id { get; set; }
         public string Name { get; set; }
         public int  Age { get; set; }
+        public string Attr { get; set; }
     }
 }
