0efc9dc4d031639e2c8e5c5c1489eb0895ae0d4a
俊杰 冯 <2623443095@qq.com>
2022-02-21 19:36:54 +0800

    冯俊杰在Model中的User里面把注释删掉了

--- Model/User.cs
+++ Model/User.cs
@@ -13,8 +13,6 @@ namespace Model
         public int user_Id { get; set; }//主键id
         public string user_Account { get; set; }//用户名
         public string user_Password { get; set; }//密码
-        //冯俊杰
-        //杰哥
-        //今天上课好像犯困了
+
     }
 }
