6a047f349c0623a08ff886b27ec9386714c3a089
俊杰 冯 <2623443095@qq.com>
2022-02-23 20:10:50 +0800

    冯俊杰在Model中创建了Employee

---
+++ Model/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 员工亲属详细信息表
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// 主键id
+        /// </summary>
+        [Key]
+        public int employee_Id { get; set; }
+        /// <summary>
+        /// taskid
+        /// </summary>
+        public int TaskId { get; set; }
+        /// <summary>
+        ///亲属姓名
+        /// </summary>
+        public string employee_Name { get; set; }
+        /// <summary>
+        /// 与本人关系
+        /// </summary>
+        public string employee_Relation { get; set; }
+        /// <summary>
+        /// 亲属在公司的职务
+        /// </summary>
+        public string employee_Post { get; set; }
+    }
+}
