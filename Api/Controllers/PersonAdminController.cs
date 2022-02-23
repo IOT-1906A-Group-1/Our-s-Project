0fe3e52dbb3b4f40822715cb065e526ec34fc7f6
mike Yang <15802994845@163.com>
2022-02-23 15:05:18 +0800

    杨泽霖更改了Api里的Leave控制器为PersonAdmin控制器

---
+++ Api/Controllers/PersonAdminController.cs
@@ -0,0 +1,31 @@
+﻿using BPMAPI.OtherApi;
+using bpmdemoapi.models;
+using Domain.InputModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+
+    [ApiController]
+    public class PersonAdminController : BaseController
+    {
+        public PersonAdminController(IConfiguration configuration) : base(configuration)
+        {
+           
+        }
+        [HttpPost]
+        [Route("api/stratBPM")]
+        public int stratBPM(InputLeaveNew leaveNew)
+        {
+            StartProccess(leaveNew);
+            return 1;
+        }
+    }
+}
