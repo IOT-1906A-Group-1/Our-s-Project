0ba20d74071a67eccc36fe2fe22e2f1cebb55c26
俊杰 冯 <2623443095@qq.com>
2022-02-27 21:36:49 +0800

    冯俊杰更改了Basecontroller

--- Api/Controllers/BaseController.cs
+++ Api/Controllers/BaseController.cs
@@ -9,92 +9,20 @@ using System.Data;
 using System.Data.SqlTypes;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-<<<<<<< HEAD
-=======
-using System.Text.RegularExpressions;
->>>>>>> f15dae120e83e685aaeaf81150e2341f9cf74089
 using System.Threading.Tasks;
 using System.Xml;
 
 namespace Api.Controllers
 {
-<<<<<<< HEAD
-    public class BaseController
+
+    [ApiController]
+    public class BaseController : ControllerBase
     {
-        protected DataSet dataSet = new DataSet("FormData");
-=======
-   
-    public class BaseController
-    {
-        protected  DataSet dataSet = new DataSet("FormData");
->>>>>>> f15dae120e83e685aaeaf81150e2341f9cf74089
-        private const string IsNotField = "Action,BPMUser,BPMUserPass,FullName,ProcessName,Detail";
         private IConfiguration configuration;
         public BaseController(IConfiguration configuration)
         {
             this.configuration = configuration;
         }
-<<<<<<< HEAD
-        protected string CollectionToSqlXml<T>(string json) where T : class, new()
-=======
-        protected string CollectionToSqlXml<T>(string json) where T:class,new()
->>>>>>> f15dae120e83e685aaeaf81150e2341f9cf74089
-        {
-            List<T> TCollection;
-            if (json.IndexOf("[{") > 0)
-            {
-                TCollection = JsonConvert.DeserializeObject<List<T>>(json);
-            }
-<<<<<<< HEAD
-
-=======
-            
->>>>>>> f15dae120e83e685aaeaf81150e2341f9cf74089
-            else
-            {
-                TCollection = JsonConvert.DeserializeObject<List<T>>("[" + json + "]");
-            }
-
-            //先把集合转换成数据表，然后把数据表转换成SQLXML
-<<<<<<< HEAD
-            return DataTableToSqlXml(CollectionToDataTable(TCollection)).Value.Replace("<DocumentElement>", "").Replace("</DocumentElement>", "");
-
-=======
-            return  DataTableToSqlXml(CollectionToDataTable(TCollection)).Value.Replace("<DocumentElement>", "").Replace("</DocumentElement>", "");
-            
->>>>>>> f15dae120e83e685aaeaf81150e2341f9cf74089
-        }
-        private DataTable CollectionToDataTable<T>(List<T> TCollection)
-        {
-            //获取泛型的具体类型
-            Type type = typeof(T);
-            //获取类型的公共属性
-            PropertyInfo[] properties = type.GetProperties();
-            //创建数据表，表名为类型名称
-            DataTable table = new DataTable(type.Name);
-            //把公共属性转行成表格列，再把表格列添加到表格中
-            foreach (var property in properties)
-<<<<<<< HEAD
-            {
-                //创建一个表格列，列名为属性名，列数据类型为属性的类型
-                DataColumn column = new DataColumn(property.Name, property.PropertyType);
-                //把表格列添加到表格中
-                table.Columns.Add(column);
-            }
-            //把泛型集合元素添加到数据行中
-            foreach (var item in TCollection)
-            {
-                //创建和表格行架构相同的表格行
-                DataRow row = table.NewRow();
-                //读取元素所有属性列的值，并根据属性名称，把属性值添加到表格行中
-                foreach (var property in properties)
-                    row[property.Name] = property.GetValue(item, null);
-                //把表格行添加到表格中
-                table.Rows.Add(row);
-            }
-            return table;
-        }
         /// <summary>
         /// 数据表转换成SQLXML
         /// </summary>
@@ -118,135 +46,19 @@ namespace Api.Controllers
             }
             return xml;
         }
-        ///// <summary>
-        ///// 获取table
-        ///// </summary>
-        ///// <param name="data"></param>
-        ///// <returns></returns>
-        //private static DataSet GetDataSet(Object data)
-        //{
-        //    Type type = data.GetType();
-
-        //    DataSet formDataSet = new DataSet("FormData");
-
-        //    DataTable table = new DataTable(type.Name);
-        //    string IsNotField = "Action,BPMUser,BPMUserPass,FullName,ProcessName";
-        //    foreach (var property in type.GetProperties())
-        //    {
-
-        //        if (!IsNotField.Contains(property.Name))
-        //            table.Columns.Add(new DataColumn(property.Name, property.PropertyType));
-        //    }
-        //    DataRow add_row = table.NewRow();
-        //    foreach (var property in type.GetProperties())
-        //    {
-        //        if (!IsNotField.Contains(property.Name))
-        //            add_row[property.Name] = property.GetValue(data);
-        //    }
-        //    table.Rows.Add(add_row);
-        //    formDataSet.Tables.Add(table);
-        //    return formDataSet;
-        //}
-
-
-        protected Task<int> StartProccess(string formDataSet, BaseModels baseModels)
+     
+        protected Task<int> StartProccess<T>(T leaveNew) where T : BaseModels, new()
         {
-
-
+            string formDataSet = ConvertXML.ConvertDataSetToXML(GetDataSet(leaveNew));
             BPMModels models = new BPMModels(configuration)
             {
-                Action = baseModels.Action,
-                BPMUser = baseModels.BPMUser,
-                BPMUserPass = baseModels.BPMUserPass,
-                FormDataSet = "<FormData>" + formDataSet + "</FormData>",
-=======
-            {
-                //创建一个表格列，列名为属性名，列数据类型为属性的类型
-                DataColumn column = new DataColumn(property.Name, property.PropertyType);
-                //把表格列添加到表格中
-                table.Columns.Add(column);
-            }
-            //把泛型集合元素添加到数据行中
-            foreach (var item in TCollection)
-            {
-                //创建和表格行架构相同的表格行
-                DataRow row = table.NewRow();
-                //读取元素所有属性列的值，并根据属性名称，把属性值添加到表格行中
-                foreach (var property in properties)
-                    row[property.Name] = property.GetValue(item, null);
-                //把表格行添加到表格中
-                table.Rows.Add(row);
-            }
-            return table;
-        }
-        /// <summary>
-        /// 数据表转换成SQLXML
-        /// </summary>
-        /// <param name="table">数据表</param>
-        /// <returns></returns>
-        private SqlXml DataTableToSqlXml(DataTable table)
-        {
-            SqlXml xml;
-            //如果表格名为空，则设置表格名
-            if (string.IsNullOrEmpty(table.TableName))
-                table.TableName = "TableName";
-            //把数据表转换成XML
-            using (var ms = new MemoryStream())
-            {
-                //把数据表转换成XML格式，并写入内存流
-                table.WriteXml(ms);
-                //把内存流读取标记设置回起点
-                ms.Position = 0;
-                //使用XmlReader读取内存流，并创建一个SqlXml对象
-                xml = new SqlXml(XmlReader.Create(ms));
-            }
-            return xml;
-        }
-        ///// <summary>
-        ///// 获取table
-        ///// </summary>
-        ///// <param name="data"></param>
-        ///// <returns></returns>
-        //private static DataSet GetDataSet(Object data)
-        //{
-        //    Type type = data.GetType();
+                Action = leaveNew.Action,
 
-        //    DataSet formDataSet = new DataSet("FormData");
-
-        //    DataTable table = new DataTable(type.Name);
-        //    string IsNotField = "Action,BPMUser,BPMUserPass,FullName,ProcessName";
-        //    foreach (var property in type.GetProperties())
-        //    {
-
-        //        if (!IsNotField.Contains(property.Name))
-        //            table.Columns.Add(new DataColumn(property.Name, property.PropertyType));
-        //    }
-        //    DataRow add_row = table.NewRow();
-        //    foreach (var property in type.GetProperties())
-        //    {
-        //        if (!IsNotField.Contains(property.Name))
-        //            add_row[property.Name] = property.GetValue(data);
-        //    }
-        //    table.Rows.Add(add_row);
-        //    formDataSet.Tables.Add(table);
-        //    return formDataSet;
-        //}
-
-       
-        protected Task<int> StartProccess(string formDataSet, BaseModels baseModels) 
-        {
-
-         
-            BPMModels models = new BPMModels(configuration)
-            {
-                Action = baseModels.Action,
-
-                BPMUser = baseModels.BPMUser,
-                BPMUserPass = baseModels.BPMUserPass,
-                FormDataSet = "<FormData>"+formDataSet+ "</FormData>",
->>>>>>> f15dae120e83e685aaeaf81150e2341f9cf74089
-                FullName = baseModels.FullName,
-                ProcessName = baseModels.ProcessName
+                BPMUser = leaveNew.BPMUser,
+                BPMUserPass = leaveNew.BPMUserPass,
+                FormDataSet = formDataSet,
+                FullName = leaveNew.FullName,
+                ProcessName = leaveNew.ProcessName
             };
             return MyClientApi.OptClientApi(models.BpmServerUrl, models);
         }
--- Api/Controllers/PersonManageController.cs
+++ Api/Controllers/PersonManageController.cs
@@ -25,64 +25,37 @@ namespace Api.Controllers
         {
            
         }
-<<<<<<< HEAD
-        /// <summary>
-        /// 员工录用
-        /// </summary>
-        /// <param name="inputEmploy"></param>
-        [HttpPost,Route("api/startemploy")]
-        public int StartEmploy(InputEmployAll inputEmployAll)
-        {
-            //string a = inputEmployAll.EmployData.Replace("\\", "");
-            //string b = inputEmployAll.EmployeeDetail.Replace("\\", "");
-            var xml = CollectionToSqlXml<Employ>(inputEmployAll.EmployData);
-            var xml1 = CollectionToSqlXml<Employee>(inputEmployAll.EmployeeDetail);
-            StartProccess(xml+xml1, inputEmployAll);
-=======
-        //上传文件
-        [HttpPost, Route("FileUpload")]
-        public int FileUpload(IFormFile file)
-        {
-            if (file != null)
-            {
-                var path = Directory.GetCurrentDirectory();
-                int indexDot = file.FileName.IndexOf(".");
-                int nameLength = file.FileName.Length;
-                var date = DateTime.Now;
-                fileSuffix = file.FileName.Substring(indexDot, nameLength - indexDot);
-                fileName = file.FileName.Substring(0, indexDot) + "_Time=" + date.Year + date.Month + date.Day + "_GUID=" + Guid.NewGuid().ToString("N");
-                var fileDir = path+"\\Files";
-                if (!Directory.Exists(fileDir))
-                {
-                    Directory.CreateDirectory(fileDir);
-                }
-                //文件名称
-                string projectFileName = fileName + fileSuffix;
-                filePath = fileDir + $@"\{projectFileName}";
-                using (FileStream fs = System.IO.File.Create(filePath))
-                {
-                    file.CopyTo(fs);
-                    fs.Flush();
-                }
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
         /// <summary>
         /// 请假
         /// </summary>
         /// <param name="leaveNew"></param>
         /// <returns></returns>
         [HttpPost]
-        [Route("api/stratLeave")]
-        public int stratLeave( BPMLeaveModels leave)
+        [Route("api/stratBPM")]
+        public int stratBPM(InputLeaveNew leaveNew)
         {
-            var xml = CollectionToSqlXml<Leave>(leave.LeaveData);
-            StartProccess(xml,leave);
->>>>>>> f15dae120e83e685aaeaf81150e2341f9cf74089
+            StartProccess<InputLeaveNew>(leaveNew);
+            return 1;
+        }
+        /// <summary>
+        /// 员工录用
+        /// </summary>
+        /// <param name="inputEmploy"></param>
+        [HttpPost,Route("api/startemploy")]
+        public void StartEmploy(InputEmploy inputEmploy)
+        {
+            StartProccess<InputEmploy>(inputEmploy);
+        }
+        /// <summary>
+        /// 人力资源需求
+        /// </summary>
+        /// <param name="leaveNew"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/StartHR")]
+        public int StartHR(InputHRModel leaveNew)
+        {
+            StartProccess(leaveNew);
             return 1;
         }
     }
