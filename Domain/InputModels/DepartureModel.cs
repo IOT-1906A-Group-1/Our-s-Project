f1d06ecfb76b7937d771ed4b6448a55ca601b898
西域男孩 <@LAPTOP-OCL842RC>
2022-02-27 21:11:33 +0800

    西域男孩在Domain中创建了DepartureModel ，在类InputDeparture中取消继承BaseModels

---
+++ Domain/InputModels/DepartureModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.InputModels
+{
+    public class DepartureModel:BaseModels
+    {
+        public string Departure { get; set; }
+    }
+}
