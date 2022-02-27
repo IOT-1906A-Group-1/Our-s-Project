953c03ec895bba95529c8f91bb33032704c39cc9
mike Yang <15802994845@163.com>
2022-02-27 21:10:59 +0800

    杨泽霖修改了人事管理控制器

--- Api/Controllers/PersonManageController.cs
+++ Api/Controllers/PersonManageController.cs
@@ -8,18 +8,54 @@ using Model;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Api.Controllers
 {
 
-    [ApiController]
+    [ApiController] 
     public class PersonManageController : BaseController
     {
+        static string fileName;//文件名称
+        static string filePath;//文件路径
+        static string fileSuffix;//文件后缀名
         public PersonManageController(IConfiguration configuration) : base(configuration)
         {
            
+        }
+        //上传文件
+        [HttpPost, Route("FileUpload")]
+        public int FileUpload(IFormFile file)
+        {
+            if (file != null)
+            {
+                var path = Directory.GetCurrentDirectory();
+                int indexDot = file.FileName.IndexOf(".");
+                int nameLength = file.FileName.Length;
+                var date = DateTime.Now;
+                fileSuffix = file.FileName.Substring(indexDot, nameLength - indexDot);
+                fileName = file.FileName.Substring(0, indexDot) + "_Time=" + date.Year + date.Month + date.Day + "_GUID=" + Guid.NewGuid().ToString("N");
+                var fileDir = path+"\\Files";
+                if (!Directory.Exists(fileDir))
+                {
+                    Directory.CreateDirectory(fileDir);
+                }
+                //文件名称
+                string projectFileName = fileName + fileSuffix;
+                filePath = fileDir + $@"\{projectFileName}";
+                using (FileStream fs = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
         /// <summary>
         /// 请假
@@ -27,52 +63,12 @@ namespace Api.Controllers
         /// <param name="leaveNew"></param>
         /// <returns></returns>
         [HttpPost]
-        [Route("api/stratBPM")]
-        public int stratBPM(InputLeaveNew leaveNew)
+        [Route("api/stratLeave")]
+        public int stratLeave( BPMLeaveModels leave)
         {
-            StartProccess<InputLeaveNew>(leaveNew);
+            var xml = CollectionToSqlXml<Leave>(leave.LeaveData);
+            StartProccess(xml,leave);
             return 1;
-        }
-        /// <summary>
-        /// 员工录用
-        /// </summary>
-        /// <param name="inputEmploy"></param>
-        [HttpPost,Route("api/startemploy")]
-        public void StartEmploy(InputEmploy inputEmploy)
-        {
-            StartProccess<InputEmploy>(inputEmploy);
-        }
-        /// <summary>
-        /// 人力资源需求
-        /// </summary>
-        /// <param name="leaveNew"></param>
-        /// <returns></returns>
-        [HttpPost]
-        [Route("api/StartHR")]
-        public int StartHR(InputHRModel leaveNew)
-        {
-            StartProccess(leaveNew);
-            return 1;
-        }
-
-        /// <summary>
-        /// 员工离职
-        /// </summary>
-        /// <param name="inputEmploy"></param>
-        [HttpPost, Route("api/startDepartur")]
-        public void StartDeparture(InputDeparture inputDeparture)
-        {
-            StartProccess<InputDeparture>(inputDeparture);
-        }
-        /// <summary>
-        /// 招聘计划
-        /// </summary>
-        /// <param name="models"></param>
-        [HttpPost]
-        [Route("api/StartPlan")]
-        public void StartPlan(InputPlanModels models)
-        {
-            StartProccess<InputPlanModels>(models);
         }
     }
 }
