5864ffe1c96a4da0c76f68fcebd3f4aded6b9141
西域男孩 <@LAPTOP-OCL842RC>
2022-03-01 09:50:43 +0800

    西域男孩在Model创建了ProcurementPlus和Procurement

---
+++ Model/Procurement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Procurement
+    {
+        /// <summary>
+        ///日常用品采购申请单 --主表
+        /// </summary>  
+
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int procurement_Id { get; set; }
+        /// <summary>
+        /// taskId
+        /// </summary>
+        public int TaskId { get; set; }
+        /// <summary>
+        /// 申请人
+        /// </summary>
+        public string procurement_Name { get; set; }
+        /// <summary>
+        /// 经办人
+        /// </summary>
+        public string procurement_Jing { get; set; }
+        /// <summary>
+        /// 申请部门
+        /// </summary>
+        public string procurement_Sbu { get; set; }
+        /// <summary>
+        /// 经办部门
+        /// </summary>
+        public string procurement_Jbu { get; set; }
+        /// <summary>
+        /// 申请日期
+        /// </summary>
+        public DateTime procurement_Stime { get; set; }
+        /// <summary>
+        /// 经办日期
+        /// </summary>
+        public DateTime procurement_Jtime { get; set; }
+        /// <summary>
+        /// 用途
+        /// </summary>
+        public string procurement_Yong { get; set; }
+    }
+}
---
+++ Model/ProcurementPlus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+   public  class ProcurementPlus
+    {
+
+        /// <summary>
+        /// 日常用品采购申请单副表
+        /// </summary>
+        
+        /// <summary>
+        /// 主键序号
+        /// </summary>
+        public int procurementplus_Id { get; set; }
+        /// <summary>
+        /// taskId
+        /// </summary>
+        public int TaskId { get; set; }
+        /// <summary>
+        /// 用品名称
+        /// </summary>
+        public string procurementplus_Name { get; set; }
+        /// <summary>
+        /// 规格
+        /// </summary>
+        public string procurementplus_Gui { get; set; }
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public int procurementplus_Num { get; set; }
+        /// <summary>
+        /// 单位
+        /// </summary>
+        public string procurementplus_Dan { get; set; }
+        /// <summary>
+        /// 金额
+        /// </summary>
+        public decimal procurementplus_Price { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string procurementplus_Bei { get; set; }
+
+    }
+}
