c79d224064f67cec397d5398508dea0aebb39822
俊杰 冯 <2623443095@qq.com>
2022-03-02 14:11:05 +0800

    冯俊杰新增了car服务接口

---
+++ IService/IServiceDBCar.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IService
+{
+    public interface IServiceDBCar
+    {
+        int AddCarmaintain(CarMaintain carmaintain);
+        int AddCarinsurance(CarInsurance carInsurance);
+        int AddCarservice(CarService carService);
+        int AddCarbreak(CarBreak carBreak);
+    }
+}
