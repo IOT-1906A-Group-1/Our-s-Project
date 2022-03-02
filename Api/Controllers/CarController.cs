6cc80d78e0ec554ab25b0fe17f718339b3a96a78
俊杰 冯 <2623443095@qq.com>
2022-03-02 14:08:52 +0800

    冯俊杰新增了Car控制器

---
+++ Api/Controllers/CarController.cs
@@ -0,0 +1,59 @@
+﻿using IService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        //连接服务
+        private readonly IServiceDBCar serviceDBCar;
+        //构造函数
+        public CarController(IServiceDBCar serviceDBCar)
+        {
+            this.serviceDBCar = serviceDBCar;
+        }
+        /// <summary>
+        /// 车辆保险添加函数
+        /// </summary>
+        /// <param name="carmaintain"></param>
+        /// <returns></returns>
+        [HttpPost,Route("api/carmaintain")]
+        public int CarmaintainAdd([FromBody]CarMaintain carmaintain)
+        {
+            return serviceDBCar.AddCarmaintain(carmaintain);
+        }
+        /// <summary>
+        /// 车辆保险添加函数
+        /// </summary>
+        /// <param name="carInsurance"></param>
+        /// <returns></returns>
+        [HttpPost,Route("api/carinsurance")]
+        public int CarinsuranceAdd([FromBody] CarInsurance carInsurance)
+        {
+            return serviceDBCar.AddCarinsurance(carInsurance);
+        }
+        /// <summary>
+        /// 车辆维修添加函数
+        /// </summary>
+        /// <param name="carService"></param>
+        /// <returns></returns>
+        [HttpPost,Route("api/carservice")]
+        public int CarserviceAdd([FromBody] CarService carService)
+        {
+            return serviceDBCar.AddCarservice(carService);
+        }
+        /// <summary>
+        /// 车辆违章添加函数
+        /// </summary>
+        /// <param name="carBreak"></param>
+        /// <returns></returns>
+        [HttpPost,Route("api/carbreak")]
+        public int CarbreakAdd([FromBody] CarBreak carBreak)
+        {
+            return serviceDBCar.AddCarbreak(carBreak);
+        }
+    }
+}
--- Api/Controllers/PersonManageController.cs
+++ Api/Controllers/PersonManageController.cs
@@ -18,46 +18,10 @@ namespace Api.Controllers
     [ApiController] 
     public class PersonManageController : BaseController
     {
-        static string fileName;//文件名称
-        static string filePath;//文件路径
-        static string fileSuffix;//文件后缀名
         public PersonManageController(IConfiguration configuration) : base(configuration)
         {
            
         }
-        //上传文件
-        [HttpPost, Route("FileUpload")]
-        public int FileUpload(IFormFile file)
-        {
-            if (file != null)
-            {
-                var path = Directory.GetCurrentDirectory();
-                int indexDot = file.FileName.IndexOf(".");
-                int nameLength = file.FileName.Length;
-                var date = DateTime.Now;
-                fileSuffix = file.FileName.Substring(indexDot, nameLength - indexDot);
-                fileName = file.FileName.Substring(0, indexDot) + "_Time=" + date.Year + date.Month + date.Day + "_GUID=" + Guid.NewGuid().ToString("N");
-                var fileDir = path + "\\Files";
-                if (!Directory.Exists(fileDir))
-                {
-                    Directory.CreateDirectory(fileDir);
-                }
-                //文件名称
-                string projectFileName = fileName + fileSuffix;
-                filePath = fileDir + $@"\{projectFileName}";
-                using (FileStream fs = System.IO.File.Create(filePath))
-                {
-                    file.CopyTo(fs);
-                    fs.Flush();
-                }
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
         /// <summary>
         /// 请假
         /// </summary>
@@ -118,19 +82,6 @@ namespace Api.Controllers
         {
             var xml = CollectionToSqlXml<HRModel>(leaveNew.HRdata);
             StartProccess(xml, leaveNew);
-            return 1;
-        }
-        /// <summary>
-        /// 用章申请
-        /// </summary>
-        /// <param name="models"></param>
-        /// <returns></returns>
-        [HttpPost]
-        [Route("api/StartChapter")]
-        public int StartChapter(BPMChapterModels models)
-        {
-            var UseChapter = CollectionToSqlXml<ChapterModels>(models.ChapterData);
-            StartProccess(UseChapter, models);
             return 1;
         }
     }
