dd3e1fe667779fb453e1d8f45ac023626b3c7465
西域男孩 <@LAPTOP-OCL842RC>
2022-03-02 14:29:12 +0800

    西域男孩在COntroller中写入了方法

--- Api/Controllers/MeetroomController.cs
+++ Api/Controllers/MeetroomController.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.InputModels;
+using Model;
 
 namespace Api.Controllers
 {
@@ -17,6 +19,28 @@ namespace Api.Controllers
         public MeetroomController(IConfiguration configuration) : base(configuration)
         {
 
+        }
+        /// <summary> 
+        /// 会议室新增
+        /// </summary>                                                                           
+        /// <param name="InputRoomNewModel"></param>
+        [HttpPost, Route("api/startRoomNewModel")]
+        public int StartDeparture(BPMRoomNewModel bPMRoomNewModel)
+        {
+            var xml = CollectionToSqlXml<RoomNewModel>(bPMRoomNewModel.RoomNewModel);
+            StartProccess(xml, bPMRoomNewModel);
+            return 1;
+        }
+        /// <summary>
+        /// 会议室预定
+        /// </summary>
+        /// <param name="InputRoomreservationModel"></param>
+        [HttpPost, Route("api/startRoomreservationModel")]
+        public int StartRoomreservationModel(BPMRoomreservationModel bPMRoomreservationModel)
+        {
+            var xml = CollectionToSqlXml<RoomreservationModel>(bPMRoomreservationModel.RoomreservationModel);
+            StartProccess(xml, bPMRoomreservationModel);
+            return 1;
         }
     }
 }
--- Api/Controllers/NecessitiesController.cs
+++ Api/Controllers/NecessitiesController.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.InputModels;
+using Model;
 
 namespace Api.Controllers
 {
@@ -16,7 +18,20 @@ namespace Api.Controllers
     {
         public NecessitiesController(IConfiguration configuration) : base(configuration)
         {
-
+           
+        }
+        /// <summary>
+        /// 日常用品采购
+        /// </summary>
+        /// <param name="InputProcurement"></param>
+        [HttpPost]
+        [Route("api/StarProcurement")]
+        public int StarProcurement(BPMProcurementModel models)
+        {
+            var plan = CollectionToSqlXml<Procurement>(models.Procurement);
+            var info = CollectionToSqlXml<ProcurementPlus>(models.ProcurementPlus);
+            StartProccess(plan + info,models);
+            return 1;
         }
     }
 }
