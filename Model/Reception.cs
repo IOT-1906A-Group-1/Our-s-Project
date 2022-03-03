41315d7ee41177c237ea5e1adab946335252ca55
西域男孩 <@LAPTOP-OCL842RC>
2022-03-03 19:53:02 +0800

    西域男孩在Model中创建了三个类

---
+++ Model/Reception.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Reception
+    {
+        /// <summary>
+        /// 接待申请表--主表
+        /// </summary>
+
+        /// <summary>
+        /// 主键Id
+        /// </summary>
+        [Key]
+        public int reception_Id { get; set; }
+        /// <summary>
+        /// taskid
+        /// </summary>
+        public int TaskId { get; set; }
+        /// <summary>
+        /// 申请人
+        /// </summary>
+        public string reception_Name { get; set; }
+        /// <summary>
+        /// 申请部门
+        /// </summary>
+        public string reception_Bu { get; set; }
+        /// <summary>
+        /// 申请时间
+        /// </summary>
+        public DateTime reception_Stime { get; set; }
+        /// <summary>
+        /// 来访客人单位
+        /// </summary>
+        public string reception_Laifang { get; set; }
+        /// <summary>
+        /// 主要客人姓名、职位
+        /// </summary>
+        public string reception_Zhuyao { get; set; }
+        /// <summary>
+        /// 停留时间
+        /// </summary>
+        public DateTime reception_Ttime { get; set; }
+        public DateTime reception_Ttimeplus { get; set; }
+        /// <summary>
+        /// 接待人
+        /// </summary>
+        public string reception_Jie { get; set; }
+        /// <summary>
+        /// 人数
+        /// </summary>
+        public int reception_Rnum { get; set; }
+        /// <summary>
+        /// 是否需要准备资料
+        /// </summary>
+        public bool reception_Zi { get; set; }
+        /// <summary>
+        /// 往来航班号
+        /// </summary>
+        public string reception_Lai { get; set; }
+        /// <summary>
+        /// 是否需要物品
+        /// </summary>
+        public bool reception_Wu { get; set; }
+        /// <summary>
+        /// 来访事由
+        /// </summary>
+        public string reception_Shi { get; set; }
+        /// <summary>
+        /// 其他要求
+        /// </summary>
+        public string reception_Yao { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string reception_Bei { get; set; }
+    }
+}
---
+++ Model/Receptions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Receptions
+    {
+        /// <summary>
+        /// 接待申请表-副表1
+        /// </summary>
+        
+        /// <summary>
+        /// 主键Id
+        /// </summary>
+        [Key]
+        public int receptions_Id { get; set; }
+        /// <summary>
+        /// taskid
+        /// </summary>
+        public int TaskId { get; set; }
+        /// <summary>
+        /// 物品名称
+        /// </summary>
+        public string receptions_Name { get; set; }
+        /// <summary>
+        /// 规格
+        /// </summary>
+        public string receptions_Ben { get; set; }
+        /// <summary>
+        /// 使用数量
+        /// </summary>
+        public string receptions_Qin { get; set; }
+        /// <summary>
+        /// 库存数量
+        /// </summary>
+        public string receptions_Knum { get; set; }
+        /// <summary>
+        /// 剩余数量
+        /// </summary>
+        public string receptions_Snum { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string receptions_Bei { get; set; }
+    }
+}
