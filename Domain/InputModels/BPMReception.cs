f7cd8179d96b1324fd505ae2f038f15f34c9b8f7
西域男孩 <@LAPTOP-OCL842RC>
2022-03-03 19:53:35 +0800

    西域男孩在Domain/InpuModel中创建了四个类

---
+++ Domain/InputModels/BPMReception.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.InputModels
+{
+    public class BPMReception:BaseModels
+    {
+        /// <summary>
+        /// 接待申请表--主表
+        /// </summary>
+        public string Reception { get; set; }
+        /// <summary>
+        /// 接待申请表-副表2
+        /// </summary>
+        public string Receptions { get; set; }
+        /// <summary>
+        /// 接待申请表-副表3
+        /// </summary>
+        public string Receptionplus { get; set; }
+    }
+}
---
+++ Domain/InputModels/InputReceptions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.InputModels
+{
+    public class InputReceptions
+    {
+        /// <summary>
+        /// 接待申请表-副表2
+        /// </summary>
+
+        /// <summary>
+        /// 主键Id
+        /// </summary>
+        [Key]
+        public int receptionplus_Id { get; set; }
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public DateTime receptionplus_Stime { get; set; }
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public DateTime receptionplus_Etime { get; set; }
+        /// <summary>
+        /// 行程
+        /// </summary>
+        public string receptionplus_Xing { get; set; }
+        /// <summary>
+        /// 陪同人员
+        /// </summary>
+        public string receptionplus_Pei { get; set; }
+        /// <summary>
+        /// 是否用车
+        /// </summary>
+        public bool receptionplus_Yong { get; set; }
+        /// <summary>
+        /// 是否预定会议室
+        /// </summary>
+        public bool receptionplus_Hui { get; set; }
+        /// <summary>
+        /// 是否预定酒店
+        /// </summary>
+        public bool receptionplus_Jiu { get; set; }
+        /// <summary>
+        /// 是否需要展厅
+        /// </summary>
+        public bool receptionplus_Zhan { get; set; }
+        /// <summary>
+        /// 是否需要订餐
+        /// </summary>
+        public bool receptionplus_Can { get; set; }
+        /// <summary>
+        /// 残疾公司那些部门
+        /// </summary>
+        public string receptionplus_Bu { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string receptionplus_Bei { get; set; }
+    }
+}
