32a4d25b2b758bb4f4578a55c68875dd84e5b9ed
俊杰 冯 <2623443095@qq.com>
2022-03-03 19:58:56 +0800

    冯俊杰新增了服务接口

--- IService/IServiceDBCar.cs
+++ IService/IServiceDBCar.cs
@@ -13,5 +13,13 @@ namespace IService
         int AddCarinsurance(CarInsurance carInsurance);
         int AddCarservice(CarService carService);
         int AddCarbreak(CarBreak carBreak);
+        //int DeleteCarmaintain(int id);
+        //int DeleteCarinsurance(int id);
+        //int DeleteCarservice(int id);
+        //int DeleteCarbreak(int id);
+        //int UpdateCarmaintain(CarMaintain carMaintain);
+        //int UpdateCarinsurance(CarInsurance carInsurance);
+        //int UpdateCarservice(CarService carService);
+        //int UpdateCarbreak(CarBreak carBreak);
     }
 }
